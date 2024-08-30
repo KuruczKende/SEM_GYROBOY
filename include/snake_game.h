ee24a39e31c89c4f5003bc569d70428da1d7515d
KuruczKende <83708847+KuruczKende@users.noreply.github.com>
2024-08-30 22:20:05 +0200

    Create snake_game.h
    Start of the snake game.

---
+++ include/snake_game.h
@@ -0,0 +1,33 @@
+#ifndef SNAKE_GAME_H
+#define SNAKE_GAME_H
+
+#include "game_template.h"
+
+class C_SNAKE_GAME : public C_GAME
+{
+private:
+struct C_POS{
+    uint8_t u8PosX;
+    uint8_t u8PosY;
+}
+C_POS* pcBuffer;
+uint8_t u8HeadIdx;
+uint8_t u8Length;
+C_POS cApple;
+E_DIRECTIONS eDir;
+
+void vMakeBuffer();
+void vPlaceApple();
+void vMove();
+void vShow();
+void vGameOver();
+
+public:
+    C_SNAKE_GAME();
+    ~C_SNAKE_GAME();
+    bool boInputHandler(E_DIRECTIONS eDir, bool boButton0, bool boButton1);
+    void vGameInit();
+    void vGameLoop();
+};
+
+#endif
